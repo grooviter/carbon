2988f39de029a912214633e30d3fe6f36a80a0f1
Mario Garcia <mario.ggar@gmail.com>
2018-06-24 00:33:15 +0200

    Fix type detection by using a later compilation phase

--- src/main/groovy/carbon/ast/CarbonASTTransformation.groovy
+++ src/main/groovy/carbon/ast/CarbonASTTransformation.groovy
@@ -11,7 +11,7 @@ import org.codehaus.groovy.control.CompilePhase
  * @since 0.1.0
  */
 @CompileStatic
-@Phase(CompilePhase.CONVERSION)
+@Phase(CompilePhase.CANONICALIZATION)
 class CarbonASTTransformation extends AbstractGlobalTransformation {
     @Override
     List<Class<Transformer>> getTransformers() {
