5d70afecafa71a69c582f23e5cee8a69804158d1
Mario Garcia <mario.ggar@gmail.com>
2018-12-31 17:53:44 +0100

    Add logger to show tabular data

--- src/main/groovy/carbon/log/LoggerCli.groovy
+++ src/main/groovy/carbon/log/LoggerCli.groovy
@@ -23,6 +23,29 @@ class LoggerCli {
         return this
     }
 
+    @groovy.transform.CompileStatic
+    LoggerCli table(List<Map> rows) {
+        Integer columns = rows.first().keySet().size()
+        Set<String> knames = rows.first().keySet()
+        String headers = knames.collect({ it.center(30) }).join('|')
+        String headersLine =  (1..columns).collect({ ('-' * 30) }).join('+')
+
+        logln(headers)
+        logln(headersLine)
+
+        rows.each { Map m ->
+            String row = m
+                .keySet()
+                .collect({ k ->
+                    m[k].toString().take(30).padRight(30)
+                }).join('|')
+
+            logln(row)
+        }
+
+        return this
+    }
+
     LoggerCli clear() {
         Ansi ansi = ansi()
             .cursorUp(BEGINNING)
